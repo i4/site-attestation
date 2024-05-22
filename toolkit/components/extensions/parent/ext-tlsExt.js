82bdb3a2a061d0c9eb761026d361f814304cd78c
Luca Preibsch <luca.preibsch@fau.de>
2024-05-22 15:02:13 +0200

    modify function implementation, remove event

--- toolkit/components/extensions/parent/ext-tlsExt.js
+++ toolkit/components/extensions/parent/ext-tlsExt.js
@@ -1,22 +1,57 @@
+// this.tlsExt = class extends ExtensionAPI {
+//   getAPI(context) {
+//     return {
+//       tlsExt: {
+//         getTlsExtensionSupport(extension) {
+//           throw new ExtensionError("Dies ist ein Test");
+//           return extension;
+//         },
+//         onTlsExtensionHandle: new EventManager({
+//           context,
+//           name: "tlsExt.onTlsExtensionHandle",
+//           register: fire => {
+//             const callback = value => {
+//               fire.async(value);
+//             };
+//             RegisterSomeInternalCallback(callback);
+//             return () => {
+//               UnregisterInternalCallback(callback);
+//             };
+//           }
+//         }).api(),
+//       }
+//     }
+//   }
+// }
+
+console.log("WIR WERDEN AUSGEFÜHRT!");
+
+// this.tlsExt = class extends ExtensionAPI {
+//   getAPI(context) {
+//     return {
+//       tlsExt: {
+//         getTlsExtensionSupport(extension) {
+//           if (extension < 0) {
+//             throw new ExtensionError("Cannot");
+//           }
+//           return extension;
+//         }
+//       }
+//     }
+//   }
+// }
+
+
 this.tlsExt = class extends ExtensionAPI {
   getAPI(context) {
     return {
       tlsExt: {
-        onTlsExtensionHandle: new EventManager({
-          context,
-          name: "tlsExt.onTlsExtensionHandle",
-          register: fire => {
-            const callback = value => {
-              fire.async(value);
-            };
-            RegisterSomeInternalCallback(callback);
-            return () => {
-              UnregisterInternalCallback(callback);
-            };
-          }
-        }).api(),
-        getTlsExtensionSupport(extension) { return extension; }
-      }
-    }
+        getSecurityInfo: function (extension) {
+          if (extension < 0)
+            throw new ExtensionError("Cannot");
+          return extension;
+        },
+      },
+    };
   }
-}
+};
