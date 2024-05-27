448e49437d03297faf1ed0e0c62ba8167e63479c
Luca Preibsch <luca.preibsch@fau.de>
2024-05-27 15:22:47 +0200

    fix API GetExtensionSupport call
    Somehow the build system won't pick up the .cpp implementation, so I added the implementation to the header for now.

--- toolkit/components/extensions/tlsext/TlsExtensionService.cpp
+++ toolkit/components/extensions/tlsext/TlsExtensionService.cpp
@@ -1,10 +1,24 @@
 #include "mozilla/extensions/TlsExtensionService.h"
 
+#include "mozilla/ClearOnShutdown.h"
+
 #include "sslexp.h"
 
 namespace mozilla::extensions {
 
 NS_IMPL_ISUPPORTS(TlsExtensionService, nsITlsExtensionService)
+
+// /* static */
+// already_AddRefed<TlsExtensionService>
+// TlsExtensionService::GetSingleton() {
+//   static RefPtr<TlsExtensionService> sSingleton;
+//   if (!sSingleton) {
+//     sSingleton = new TlsExtensionService();
+//     // sSingleton->Init();
+//     ClearOnShutdown(&sSingleton);
+//   }
+//   return do_AddRef(sSingleton);
+// }
 
 NS_IMETHODIMP
 TlsExtensionService::GetExtensionSupport(uint16_t extension, nsITlsExtensionService::SSLExtensionSupport *_retval) {
--- toolkit/components/extensions/tlsext/TlsExtensionService.h
+++ toolkit/components/extensions/tlsext/TlsExtensionService.h
@@ -4,20 +4,46 @@
 
 #include "nsITlsExtensionService.h"
 
+#include "mozilla/ClearOnShutdown.h"
+
+#include "sslexp.h"
+
 
 namespace mozilla::extensions {
 
 class TlsExtensionService final : public nsITlsExtensionService {
+    public:
+
     NS_DECL_ISUPPORTS
     NS_DECL_NSITLSEXTENSIONSERVICE
 
-    public:
+    // static already_AddRefed<TlsExtensionService> GetSingleton();
+
     TlsExtensionService() = default;
 
     private:
-    // A private destructor must be declared.
     ~TlsExtensionService() = default;
 };
+
+NS_IMPL_ISUPPORTS(TlsExtensionService, nsITlsExtensionService)
+
+// /* static */
+// already_AddRefed<TlsExtensionService>
+// TlsExtensionService::GetSingleton() {
+//   static RefPtr<TlsExtensionService> sSingleton;
+//   if (!sSingleton) {
+//     sSingleton = new TlsExtensionService();
+//     // sSingleton->Init();
+//     ClearOnShutdown(&sSingleton);
+//   }
+//   return do_AddRef(sSingleton);
+// }
+
+NS_IMETHODIMP
+TlsExtensionService::GetExtensionSupport(uint16_t extension, nsITlsExtensionService::SSLExtensionSupport *_retval) {
+    SSL_GetExtensionSupport(extension, _retval);
+    return NS_OK;
+}
 
 }
 
