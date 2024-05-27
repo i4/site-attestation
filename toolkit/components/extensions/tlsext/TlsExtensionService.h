1ba684a29a3beab5bd9492f71098385d7e61f98a
Luca Preibsch <luca.preibsch@fau.de>
2024-05-27 15:47:15 +0200

    move API implementation from header file to .cpp file
    a mystery line was missing in the moz.build file, copied it from the webrequest API implementation

--- toolkit/components/extensions/tlsext/TlsExtensionService.h
+++ toolkit/components/extensions/tlsext/TlsExtensionService.h
@@ -25,26 +25,6 @@ class TlsExtensionService final : public nsITlsExtensionService {
     ~TlsExtensionService() = default;
 };
 
-NS_IMPL_ISUPPORTS(TlsExtensionService, nsITlsExtensionService)
-
-// /* static */
-// already_AddRefed<TlsExtensionService>
-// TlsExtensionService::GetSingleton() {
-//   static RefPtr<TlsExtensionService> sSingleton;
-//   if (!sSingleton) {
-//     sSingleton = new TlsExtensionService();
-//     // sSingleton->Init();
-//     ClearOnShutdown(&sSingleton);
-//   }
-//   return do_AddRef(sSingleton);
-// }
-
-NS_IMETHODIMP
-TlsExtensionService::GetExtensionSupport(uint16_t extension, nsITlsExtensionService::SSLExtensionSupport *_retval) {
-    SSL_GetExtensionSupport(extension, _retval);
-    return NS_OK;
-}
-
 }
 
 
