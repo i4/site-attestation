f43cec202f82ed1c481cadc416c9ceade3f42d83
Luca Preibsch <luca.preibsch@fau.de>
2024-06-16 10:56:18 +0200

    fix JSM component implementation

--- toolkit/components/extensions/tlsext/TlsExtensionObserver.sys.mjs
+++ toolkit/components/extensions/tlsext/TlsExtensionObserver.sys.mjs
@@ -8,6 +8,8 @@ export function TlsExtensionObserver() {
 
 // implement addListener functions here
 TlsExtensionObserver.prototype = {
+    classID: Components.ID("{d4834e5a-41db-4f97-a45d-d4dc53057553}"),
+
     setWriteTlsExtensionCallback(callback) {
         this.onWriteTlsExtension = callback;
     },
