b803ba3913e60a0d82cf7b06734b4c7ab62366df
Maxim Onciul <onciul@cs.fau.de>
2024-06-28 12:13:58 +0200

    up: report script

--- create-hash.sh
+++ create-hash.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-NONCE=$1
-PUBKEY=$(openssl x509 -pubkey -in /usr/local/nginx/cert.pem | rg --multiline '\-\-\-\-\-BEGIN PUBLIC KEY\-\-\-\-\-[\d\D]*\-\-\-\-\-END PUBLIC KEY\-\-\-\-\-')
+NONCE=cat $1
+FNAME=$2
 
+if [ -f pub.key ]; then
+  PUBKEY=$(cat pub.key)
+else
+  PUBKEY=$(openssl x509 -pubkey -in /usr/local/nginx/cert.pem | rg --multiline '\-\-\-\-\-BEGIN PUBLIC KEY\-\-\-\-\-[\d\D]*\-\-\-\-\-END PUBLIC KEY\-\-\-\-\-')
+  echo -n $PUBKEY > pub.key
+fi
+
+echo -n "$NONCE\n$PUBKEY"
+echo -n "$NONCE\n$PUBKEY" | sha512sum
 echo -n "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}'
+echo -n "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p
+# echo -n "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p > $FNAME
 
-# sha512sum 
