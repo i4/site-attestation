cc3b75fb0e0f1ee85294ac470139955b18b7a47a
Maxim Onciul <onciul@cs.fau.de>
2024-06-28 11:01:07 +0200

    feat: initial shot on create-hash.sh

---
+++ create-hash.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+NONCE=$1
+PUBKEY=$(openssl x509 -pubkey -in /usr/local/nginx/cert.pem | rg --multiline '\-\-\-\-\-BEGIN PUBLIC KEY\-\-\-\-\-[\d\D]*\-\-\-\-\-END PUBLIC KEY\-\-\-\-\-')
+
+echo -e "$NONCE\n$PUBKEY"
+
+# sha512sum 
