969c86d1cca845e0f961b2274b47a6bdd7e42eac
Maxim Onciul <onciul@cs.fau.de>
2024-06-28 11:50:11 +0200

    up: print sha512 only (still hex)

--- create-hash.sh
+++ create-hash.sh
@@ -3,6 +3,6 @@
 NONCE=$1
 PUBKEY=$(openssl x509 -pubkey -in /usr/local/nginx/cert.pem | rg --multiline '\-\-\-\-\-BEGIN PUBLIC KEY\-\-\-\-\-[\d\D]*\-\-\-\-\-END PUBLIC KEY\-\-\-\-\-')
 
-echo "$NONCE\n$PUBKEY" | sha512sum -b
+echo -n "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}'
 
 # sha512sum 
