7f9eb288fd62899ccd12c5f051f9ab82e7b2d700
Maxim Onciul <onciul@cs.fau.de>
2024-07-01 13:41:11 +0200

    debug: print bin

--- create-hash.sh
+++ create-hash.sh
@@ -8,10 +8,11 @@ echo NONCE: ${NONCE}
 echo HASHFILE: ${HASHFILE}
 echo OUTFILE: ${OUTFILE}
 
-echo "${NONCE}\n$(cat pub.key)" 
-echo "${NONCE}\n$(cat pub.key)" | sha512sum 
+echo "${NONCE}\n$(cat pub.key)"
+echo "${NONCE}\n$(cat pub.key)" | sha512sum
 echo "${NONCE}\n$(cat pub.key)" | sha512sum | awk '{print $1}'
 echo "${NONCE}\n$(cat pub.key)" | sha512sum | awk '{print $1}' | xxd -r -p > ${HASHFILE}
+echo "${NONCE}\n$(cat pub.key)" | sha512sum | awk '{print $1}' | xxd -r -p
 touch ${OUTFILE}
 
 /home/ubuntu/snpguest/target/debug/snpguest report ${OUTFILE} ${HASHFILE}
