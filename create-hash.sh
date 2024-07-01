ab931eb97faa5454f2bcf3bd6937aa41b7d0f910
Maxim Onciul <onciul@cs.fau.de>
2024-07-01 13:04:45 +0200

    fix: insert newlines in pubkey

--- create-hash.sh
+++ create-hash.sh
@@ -4,17 +4,17 @@ if [ -f pub.key ]; then
   PUBKEY=$(cat pub.key)
 else
   PUBKEY=$(openssl x509 -pubkey -in /usr/local/nginx/cert.pem | rg --multiline '\-\-\-\-\-BEGIN PUBLIC KEY\-\-\-\-\-[\d\D]*\-\-\-\-\-END PUBLIC KEY\-\-\-\-\-')
-  echo -n $PUBKEY > pub.key
+  echo $PUBKEY > pub.key
 fi
 
 echo OUTFILE: $OUTFILE
 echo HASHFILE: $HASHFILE
 echo PUBKEY: $PUBKEY
 
-# echo -n "$NONCE\n$PUBKEY"
-# echo -n "$NONCE\n$PUBKEY" | sha512sum
-# echo -n "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}'
-# echo -n "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p
-echo -n "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p > $HASHFILE
+# echo "$NONCE\n$PUBKEY"
+# echo "$NONCE\n$PUBKEY" | sha512sum
+# echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}'
+# echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p
+echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p > $HASHFILE
 
 /home/ubuntu/snpguest/target/debug/snpguest report $OUTFILE $HASHFILE
