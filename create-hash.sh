1953e5769ba912e22883ad2c7fdb5b93161fd966
Maxim Onciul <onciul@cs.fau.de>
2024-07-01 13:31:09 +0200

    fix: touch reportfile in script

--- create-hash.sh
+++ create-hash.sh
@@ -3,17 +3,8 @@
 if [ ! -f pub.key ]; then
   openssl x509 -noout -pubkey -in /usr/local/nginx/cert.pem &2>1 > pub.key
 fi
-PUBKEY=$(cat pub.key)
 
-echo OUTFILE: $OUTFILE
-echo HASHFILE: $HASHFILE
-echo PUBKEY: $PUBKEY
-
-echo "$NONCE\n$(cat pub.key)"
-echo '$NONCE\n$(cat pub.key)'
-echo "$NONCE\n$PUBKEY" | sha512sum
-echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}'
-echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p
-echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p > $HASHFILE
+echo "$NONCE\n$(cat pub.key)" | sha512sum | awk '{print $1}' | xxd -r -p > $HASHFILE
+touch $OUTFILE
 
 /home/ubuntu/snpguest/target/debug/snpguest report $OUTFILE $HASHFILE
