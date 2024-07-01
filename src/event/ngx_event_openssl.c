66bd499aa606c4bcde12fa2dd432c66c28326865
Maxim Onciul <onciul@cs.fau.de>
2024-07-01 16:15:45 +0200

    debug: output num bytes read from report to buffer

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -380,7 +380,7 @@ static int callbackAddExtensionRAServer(SSL *ssl, unsigned int extType,
 
             fclose(outfile);
 
-            printf("TLS::ServerCertificate:\n%s\n", ctx->attestation_report_buffer);
+            printf("TLS::ServerCertificate:\n%s\nWritten: %ul\n", ctx->attestation_report_buffer, written);
 
             *out = (unsigned char*) ctx->attestation_report_buffer;
             *outlen = written;
