60b6f7bee09d9c49f7560cbb96cc669eb32a9cc0
Maxim Onciul <onciul@cs.fau.de>
2024-07-01 13:48:26 +0200

    debug: use shellcheck

--- create-hash.sh
+++ create-hash.sh
@@ -4,17 +4,14 @@ if [ ! -f pub.key ]; then
   openssl x509 -noout -pubkey -in /usr/local/nginx/cert.pem > pub.key
 fi
 
-echo NONCE: ${NONCE}
-echo HASHFILE: ${HASHFILE}
-echo OUTFILE: ${OUTFILE}
+echo NONCE: "${NONCE}"
+echo HASHFILE: "${HASHFILE}"
+echo OUTFILE: "${OUTFILE}"
 
-echo PWD: $(pwd)
+echo PWD: "$(pwd)"
 
-echo "${NONCE}\n$(cat pub.key)"
-echo "${NONCE}\n$(cat pub.key)" | sha512sum
-echo "${NONCE}\n$(cat pub.key)" | sha512sum | awk '{print $1}'
-echo "${NONCE}\n$(cat pub.key)" | sha512sum | awk '{print $1}' | xxd -r -p > ${HASHFILE}
-echo "${NONCE}\n$(cat pub.key)" | sha512sum | awk '{print $1}' | xxd -r -p
-touch ${OUTFILE}
+printf "%s\n%s" "${NONCE}" "$(cat pub.key)"
+printf "%s\n%s" "${NONCE}" "$(cat pub.key)" | sha512sum | awk '{print $1}' | xxd -r -p > "${HASHFILE}"
+touch "${OUTFILE}"
 
-/home/ubuntu/snpguest/target/debug/snpguest report ${OUTFILE} ${HASHFILE}
+/home/ubuntu/snpguest/target/debug/snpguest report "${OUTFILE}" "${HASHFILE}"
