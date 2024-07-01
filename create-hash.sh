9366796d55951ee7aa6306b51ee8cd4151175c50
Maxim Onciul <onciul@cs.fau.de>
2024-07-01 13:11:50 +0200

    debug: script: -verbosity, save additional rg

--- create-hash.sh
+++ create-hash.sh
@@ -3,7 +3,7 @@
 if [ -f pub.key ]; then
   PUBKEY=$(cat pub.key)
 else
-  PUBKEY=$(openssl x509 -pubkey -in /usr/local/nginx/cert.pem | rg --multiline '\-\-\-\-\-BEGIN PUBLIC KEY\-\-\-\-\-[\d\D]*\-\-\-\-\-END PUBLIC KEY\-\-\-\-\-')
+  PUBKEY=$(openssl x509 -noout -pubkey -in /usr/local/nginx/cert.pem)
   echo $PUBKEY > pub.key
 fi
 
@@ -11,10 +11,10 @@ echo OUTFILE: $OUTFILE
 echo HASHFILE: $HASHFILE
 echo PUBKEY: $PUBKEY
 
-# echo "$NONCE\n$PUBKEY"
-# echo "$NONCE\n$PUBKEY" | sha512sum
-# echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}'
-# echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p
+echo "$NONCE\n$PUBKEY"
+echo "$NONCE\n$PUBKEY" | sha512sum
+echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}'
+echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p
 echo "$NONCE\n$PUBKEY" | sha512sum | awk '{print $1}' | xxd -r -p > $HASHFILE
 
 /home/ubuntu/snpguest/target/debug/snpguest report $OUTFILE $HASHFILE
