4d9b97a0a492d66532ca263674ebfd12405e28a0
Maxim Onciul <onciul@cs.fau.de>
2024-07-01 14:13:47 +0200

    fix: move directories to /usr/local/nginx

--- create-hash.sh
+++ create-hash.sh
@@ -6,16 +6,16 @@ if [ ! -f pub.key ]; then
   openssl x509 -noout -pubkey -in /usr/local/nginx/cert.pem > pub.key
 fi
 
-echo NONCE: "${NONCE}"
-echo HASHFILE: "${HASHFILE}"
-echo OUTFILE: "${OUTFILE}"
+echo NONCE: "/usr/local/nginx/${NONCE}"
+echo HASHFILE: "/usr/local/nginx/${HASHFILE}"
+echo OUTFILE: "/usr/local/nginx/${OUTFILE}"
 
-touch "${OUTFILE}"
+touch "/usr/local/nginx/${OUTFILE}"
 
 echo PWD: "$(pwd)"
 
 printf "%s\n%s" "${NONCE}" "$(cat pub.key)"
-printf "%s\n%s" "${NONCE}" "$(cat pub.key)" | sha512sum | awk '{print $1}' | xxd -r -p | tee "${HASHFILE}"
+printf "%s\n%s" "${NONCE}" "$(cat pub.key)" | sha512sum | awk '{print $1}' | xxd -r -p | tee "/usr/local/nginx/${HASHFILE}"
 
 
-/home/ubuntu/snpguest/target/debug/snpguest report "${OUTFILE}" "${HASHFILE}"
+/home/ubuntu/snpguest/target/debug/snpguest report "/usr/local/nginx/${OUTFILE}" "/usr/local/nginx/${HASHFILE}"
--- pull_snp.sh
+++ pull_snp.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+set -eu
 
 git pull origin filein-fileout
 git checkout origin/filein-fileout
+
+pushd /usr/local/nginx
 mkdir requests reports hashes
+popd
 
 make
 sudo objs/nginx
