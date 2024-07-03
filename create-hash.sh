7b88f1d7c0526ef1d8e3408e84223c5fe6e740a7
Maxim Onciul <onciul@cs.fau.de>
2024-07-03 15:09:34 +0200

    debug: save the full challenge in a file to compare to client

--- create-hash.sh
+++ create-hash.sh
@@ -3,6 +3,7 @@
 set -eu
 
 PUBKEY_PATH=/usr/local/nginx/pubkey.pem
+CHALLENGE_PATH="/usr/local/nginx/challenges/$(basename ${HASHFILE})"
 
 if [ ! -f pub.key ]; then
   openssl x509 -noout -pubkey -in /usr/local/nginx/cert.pem > "${PUBKEY_PATH}"
@@ -10,6 +11,7 @@ fi
 
 touch "${OUTFILE}"
 
-printf "%s\n%s" "${NONCE}" "$(cat ${PUBKEY_PATH})" | sha512sum | awk '{print $1}' | xxd -r -p > "${HASHFILE}"
+printf "%s\n%s" "${NONCE}" "$(cat ${PUBKEY_PATH})" > "${CHALLENGE_PATH}"
+sha512sum "${CHALLENGE_PATH}" | awk '{print $1}' | xxd -r -p > "${HASHFILE}"
 
 /home/ubuntu/snpguest/target/debug/snpguest report "${OUTFILE}" "${HASHFILE}"
