7330d9cd3e8a14cfada159e67380b423fa47f7b9
Luca Preibsch <luca.preibsch@fau.de>
2024-07-08 14:39:04 +0200

    Clean up and rework the API design

--- toolkit/components/extensions/parent/ext-tlsExt.js
+++ toolkit/components/extensions/parent/ext-tlsExt.js
@@ -1,27 +1,60 @@
-const SSLExtensionSupport = ["ssl_ext_none", "ssl_ext_native", "ssl_ext_native_only"];
+// from NSS sslexp.h
+const SSLExtensionSupportDict = ["ssl_ext_none", "ssl_ext_native", "ssl_ext_native_only"];
+// from NSS sslt.h
+const SSLHandshakeTypeDict = {
+  0: "ssl_hs_hello_request",
+  1: "ssl_hs_client_hello",
+  2: "ssl_hs_server_hello",
+  3: "ssl_hs_hello_verify_request",
+  4: "ssl_hs_new_session_ticket",
+  5: "ssl_hs_end_of_early_data",
+  6: "ssl_hs_hello_retry_request",
+  8: "ssl_hs_encrypted_extensions",
+  11: "ssl_hs_certificate",
+  12: "ssl_hs_server_key_exchange",
+  13: "ssl_hs_certificate_request",
+  14: "ssl_hs_server_hello_done",
+  15: "ssl_hs_certificate_verify",
+  16: "ssl_hs_client_key_exchange",
+  20: "ssl_hs_finished",
+  22: "ssl_hs_certificate_status",
+  24: "ssl_hs_key_update",
+  67: "ssl_hs_next_proto",
+  254: "ssl_hs_message_hash",           /* Not a real message. */
+  257: "ssl_hs_ech_outer_client_hello"  /* Not a real message. */
+};
+// from NSS seccommon.h
+const SECStatusDict = {
+  "SECWouldBlock": -2,
+  "SECFailure": -1,
+  "SECSuccess": 0
+};
 
 function createWriteObserver(fire) {
   return function (extension, tlsSessionId, url, messageType, maxDataLen) {
     console.log("writer called");
     if (fire !== null) {
       console.log(messageType);
-      let promise = fire.async(messageType, maxDataLen, { "sessionId": tlsSessionId, "url": url, "extension": extension });
-      // promise.then(console.log);
-      return promise; // TODO parse return, pass arguments
+      return fire.async(
+        SSLHandshakeTypeDict[messageType],
+        maxDataLen,
+        { "sessionId": tlsSessionId, "url": url, "extension": extension });
     }
-    return null; // TODO return Promise(null)?
+    return Promise.resolve(null);
   };
 }
 
 function createHandleObserver(fire) {
-  return function (extension, tlsSessionId, url, messageType, data) {
+  return async function (extension, tlsSessionId, url, messageType, data) {
     console.log("handler called");
     if (fire !== null) {
-      let promise = fire.async(messageType, data, { "sessionId": tlsSessionId, "url": url, "extension": extension });
-      // promise.then(console.log);
-      return promise; // TODO parse return, pass arguments
+      const secStatus = await fire.async(
+        SSLHandshakeTypeDict[messageType],
+        data,
+        { "sessionId": tlsSessionId, "url": url, "extension": extension });
+      return SECStatusDict[secStatus];
     }
-    return null; // TODO return Promise(null)?
+    return Promise.resolve(SECStatusDict.SECSuccess);
   };
 }
 
@@ -30,22 +63,14 @@ this.tlsExt = class extends ExtensionAPI {
   getAPI(context) {
     return {
       tlsExt: {
-        getTlsExtensionSupport: (extension) => SSLExtensionSupport[Services.tlsExtensions.getExtensionSupport(extension)],
+        getTlsExtensionSupport: (extension) => SSLExtensionSupportDict[Services.tlsExtensions.getExtensionSupport(extension)],
 
         onWriteTlsExtension: new EventManager({
           context,
           name: "tlsExt.onWriteTlsExtension",
           register: (fire, urlPattern, extension) => {
-            // const observer = createObserver();
-            // observer.setWriteTlsExtensionCallback(fire);
-
-            console.log("prior has");
-            console.log(Services.tlsExtensions.hasWriterObserver(extension));
             const observer = createWriteObserver(fire);
-            console.log(observer);
             Services.tlsExtensions.addWriterObserver(urlPattern, extension, observer);
-            console.log("posterior has");
-            console.log(Services.tlsExtensions.hasWriterObserver(extension));
             return () => {
               Services.tlsExtensions.removeWriterObserver(extension);
             };
@@ -56,10 +81,8 @@ this.tlsExt = class extends ExtensionAPI {
           context,
           name: "tlsExt.onHandleTlsExtension",
           register: (fire, urlPattern, extension) => {
-            console.log(Services.tlsExtensions.hasHandlerObserver(extension));
             const observer = createHandleObserver(fire);
             Services.tlsExtensions.addHandlerObserver(urlPattern, extension, observer);
-            console.log(Services.tlsExtensions.hasHandlerObserver(extension));
             return () => {
               Services.tlsExtensions.removeHandlerObserver(extension);
             };
