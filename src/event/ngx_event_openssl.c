ec2ef053f8729d83d68fdee197947b05cb6b7d20
Maxim Ritter von Onciul <onciul@cs.fau.de>
2024-10-23 14:32:08 +0200

    debug: unsigned char to string

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -550,7 +550,7 @@ static int callbackParseExtensionRAServer(SSL *ssl, unsigned int extType,
             for (size_t i = 0; i < inlen; i++) {
                 snprintf(&(ctx->outfile[strlen(ctx->outfile)]), 3, "%02X", in[i]);
                 snprintf(&(ctx->hashfile[strlen(ctx->hashfile)]), 3, "%02X", in[i]);
-                snprintf(&(ctx->challenge[strlen(ctx->challenge)]), 2, "%x", in[i]);
+                snprintf(&(ctx->challenge[strlen(ctx->challenge)]), 2, "%s", &in[i]);
             }
 
             return 1;
