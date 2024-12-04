2a2a30d176e27b3dd46670f4a631642dd4827da1
Luca Preibsch <luca.preibsch@fau.de>
2024-12-04 14:42:31 +0100

    implement testcases raw and unknown

--- evaluation/selenium/ratls-benchmark.py
+++ evaluation/selenium/ratls-benchmark.py
@@ -1,12 +1,22 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# global config
+url = "https://localhost"
+number_of_tests = 10
+testcases = ["unknown", "raw"] # "known"
+condition = EC.title_is("404 Not Found")
 
 # Set the path to your custom Firefox binary
 custom_firefox_path = "/Users/luca/Dev/Firefox/obj-aarch64-apple-darwin24.0.0/dist/Nightly.app/Contents/MacOS/firefox"
 
 # Set the custom Firefox profile to trust the mini ca
-profile_name = "evaluation"
 profile_path = "./profiles/evaluation-minica-profile"
 
 # Set the custom extension path
@@ -20,7 +30,38 @@ options.binary_location = custom_firefox_path
 # Set profile to start with
 options.profile = profile_path
 
-driver = webdriver.Firefox(service=service, options=options)
-driver.install_addon(extension_path, temporary=True)
+loading_times = {}
 
-driver.get("https://localhost")
+### MEASUREMENTS ###
+for testcase in testcases:
+    loading_times[testcase] = []
+    for repetition in range(number_of_tests):
+        if repetition % (number_of_tests / 10) == 0:
+            print(f"repetition: {repetition} of {number_of_tests}")
+
+        driver = webdriver.Firefox(service=service, options=options)
+        wait = WebDriverWait(driver, 20)
+
+        if testcase == "unknown":
+            driver.install_addon(extension_path, temporary=True)
+
+        start_time = time.time()
+        driver.get(url)
+
+        if testcase == "unknown":
+            # click the trust button
+            button = wait.until(EC.element_to_be_clickable((By.ID, "trust-measurement-button")))
+            button.click()
+
+        wait.until(condition)
+        end_time = time.time()
+        loading_times[testcase].append(end_time - start_time)
+
+        # clean up for next run
+        driver.quit()
+
+### OUTPUT ###
+for testcase in testcases:
+    print(f"Testcase: {testcase}")
+    print(f"{loading_times[testcase] * 1000}")
+    print()
