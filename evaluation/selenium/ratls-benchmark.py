75845605e18be6807661675192b390cec411b8c1
Luca Preibsch <luca.preibsch@fau.de>
2024-12-04 14:22:10 +0100

    fix setup, such that the mini ca is trusted and the extension is loaded

--- evaluation/selenium/ratls-benchmark.py
+++ evaluation/selenium/ratls-benchmark.py
@@ -6,16 +6,19 @@ from selenium.webdriver.firefox.options import Options
 custom_firefox_path = "/Users/luca/Dev/Firefox/obj-aarch64-apple-darwin24.0.0/dist/Nightly.app/Contents/MacOS/firefox"
 
 # Set the custom Firefox profile to trust the mini ca
-profile_path = "/Users/luca/Library/Application Support/Firefox/Profiles/4k7wb8xu.evaluation"
+profile_name = "evaluation"
+profile_path = "./profiles/evaluation-minica-profile"
 
 # Set the custom extension path
 extension_path = "/Users/luca/Dev/RATLS-WebExt/build/web-ext-artifacts/17840039dd4943e1851d-1.3.0.xpi"
 
-service = Service("/opt/homebrew/bin/geckodriver")
+service = Service("/opt/homebrew/bin/geckodriver", log_path="geckodriver.log")
 options = Options()
 
 options.binary_location = custom_firefox_path
-options.set_preference("profile", profile_path)
+
+# Set profile to start with
+options.profile = profile_path
 
 driver = webdriver.Firefox(service=service, options=options)
 driver.install_addon(extension_path, temporary=True)
