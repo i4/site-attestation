98381a4528a0565fabff036762ba6b52916132ae
Maxim Ritter von Onciul <onciul@cs.fau.de>
2025-01-29 17:23:36 +0100

    ratls: add tls ext

--- src/conn.c
+++ src/conn.c
@@ -58,99 +58,138 @@ static char *srvbase, *srvend, *srvcurrent;
 void
 conn_add_servers(void)
 {
-	struct stat st;
-	int fd, len;
+    struct stat st;
+    int fd, len;
 
-	fd = open(param.servers, O_RDONLY, 0);
-	if (fd == -1)
-		panic("%s: can't open %s\n", prog_name, param.servers);
+    fd = open(param.servers, O_RDONLY, 0);
+    if (fd == -1)
+        panic("%s: can't open %s\n", prog_name, param.servers);
 
-	fstat(fd, &st);
-	if (st.st_size == 0)
-		panic("%s: file %s is empty\n", prog_name, param.servers);
+    fstat(fd, &st);
+    if (st.st_size == 0)
+        panic("%s: file %s is empty\n", prog_name, param.servers);
 
-	srvbase = (char *)mmap(0, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (srvbase == (char *)-1)
-		panic("%s: can't mmap the file: %s\n", prog_name, strerror(errno));
+    srvbase = (char *)mmap(0, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (srvbase == (char *)-1)
+        panic("%s: can't mmap the file: %s\n", prog_name, strerror(errno));
 
-	close(fd);
+    close(fd);
 
-	srvend = srvbase + st.st_size;
-	for (srvcurrent = srvbase; srvcurrent < srvend; srvcurrent += len + 1) {
-		len = strlen(srvcurrent);
-		core_addr_intern(srvcurrent, len, param.port);
-	}
-	srvcurrent = srvbase;
+    srvend = srvbase + st.st_size;
+    for (srvcurrent = srvbase; srvcurrent < srvend; srvcurrent += len + 1) {
+        len = strlen(srvcurrent);
+        core_addr_intern(srvcurrent, len, param.port);
+    }
+    srvcurrent = srvbase;
 }
+
+
+#ifdef HAVE_SSL
+
+#define EXT_RATLS 420
+
+static int callbackParseExtensionRA(SSL *ssl, unsigned int extType,
+                                    unsigned int context,
+                                    const unsigned char *in,
+                                    size_t inlen, X509 *x,
+                                    size_t chainidx,
+                                    int *al, void *parseArg) {}
+
+static void callbackFreeExtensionRA(SSL *ssl, unsigned int extType,
+                                    unsigned int context,
+                                    const unsigned char *out,
+                                    void *add_arg) {}
+
+static int callbackAddExtensionRA(SSL *ssl, unsigned int extType,
+                                  unsigned int context,
+                                  unsigned char** out,
+                                  size_t *outlen, X509 *x,
+                                  size_t chainidx,
+                                  int *al, void *addArg) {}
+#endif
+
 
 void
 conn_init(Conn *conn)
 {
-	if (param.servers) {
-		int len = strlen(srvcurrent);
-		conn->hostname = srvcurrent;
-		conn->hostname_len = len;
-		conn->fqdname = conn->hostname;
-		conn->fqdname_len = conn->hostname_len;
+    if (param.servers) {
+        int len = strlen(srvcurrent);
+        conn->hostname = srvcurrent;
+        conn->hostname_len = len;
+        conn->fqdname = conn->hostname;
+        conn->fqdname_len = conn->hostname_len;
 
-		srvcurrent += len + 1;
-		if (srvcurrent >= srvend)
-			srvcurrent = srvbase;
-	} else if (param.server_name) {
-		conn->hostname = param.server;
-		conn->hostname_len = strlen(param.server);
-		conn->fqdname = param.server_name;
-		conn->fqdname_len = strlen(param.server_name);
-	} else {
-		conn->hostname = param.server;
-		conn->hostname_len = strlen(param.server);
-		conn->fqdname = conn->hostname;
-		conn->fqdname_len = conn->hostname_len;
-	}
+        srvcurrent += len + 1;
+        if (srvcurrent >= srvend)
+            srvcurrent = srvbase;
+    } else if (param.server_name) {
+        conn->hostname = param.server;
+        conn->hostname_len = strlen(param.server);
+        conn->fqdname = param.server_name;
+        conn->fqdname_len = strlen(param.server_name);
+    } else {
+        conn->hostname = param.server;
+        conn->hostname_len = strlen(param.server);
+        conn->fqdname = conn->hostname;
+        conn->fqdname_len = conn->hostname_len;
+    }
 
-	conn->port = param.port;
-	conn->sd = -1;
-	conn->myport = -1;
-	conn->line.iov_base = conn->line_buf;
-	conn->fqdname_len = strcspn(conn->fqdname,"\r\n"); // Chomp since used in Host header record
+    conn->port = param.port;
+    conn->sd = -1;
+    conn->myport = -1;
+    conn->line.iov_base = conn->line_buf;
+    conn->fqdname_len = strcspn(conn->fqdname,
+                                "\r\n"); // Chomp since used in Host header record
 
 #ifdef HAVE_SSL
-	if (param.use_ssl) {
-		conn->ssl = SSL_new(ssl_ctx);
-		if (!conn->ssl) {
-			ERR_print_errors_fp(stderr);
-			exit(-1);
-		}
+    if (param.use_ssl) {
+        conn->ssl = SSL_new(ssl_ctx);
+        if (!conn->ssl) {
+            ERR_print_errors_fp(stderr);
+            exit(-1);
+        }
 
-		if (param.tls_server_name)
-		{
-			SSL_set_tlsext_host_name(conn->ssl, param.tls_server_name);
-		}
-		
-		if (param.ssl_cipher_list) {
-			/* set order of ciphers */
-			int ssl_err = SSL_set_cipher_list(conn->ssl, param.ssl_cipher_list);
+        if (param.tls_server_name)
+        {
+            SSL_set_tlsext_host_name(conn->ssl, param.tls_server_name);
+        }
 
-			if (DBG > 2)
-				fprintf(stderr,
-					"core_ssl_connect: set_cipher_list returned %d\n",
-					ssl_err);
-		}
-	}
+        RA_SESSION_FLAG_INDEX = SSL_get_ex_new_index(0,
+                                "remote attestation session index", NULL, NULL, NULL);
+
+        SSL_CTX_add_custom_ext(conn->ssl->ctx
+                               , EXT_RATLS, SSL_EXT_CLIENT_HELLO | SSL_EXT_TLS1_3_CERTIFICATE
+                               , callbackAddExtensionRA
+                               , callbackFreeExtensionRA
+                               , param
+                               , callbackParseExtensionRA
+                               , NULL
+                              );
+
+        if (param.ssl_cipher_list) {
+            /* set order of ciphers */
+            int ssl_err = SSL_set_cipher_list(conn->ssl, param.ssl_cipher_list);
+
+            if (DBG > 2)
+                fprintf(stderr,
+                        "core_ssl_connect: set_cipher_list returned %d\n",
+                        ssl_err);
+        }
+    }
 #endif
 }
 
 void
 conn_deinit(Conn *conn)
 {
-	assert(conn->sd < 0 && conn->state != S_FREE);
-	assert(!conn->sendq);
-	assert(!conn->recvq);
-	assert(!conn->watchdog);
-	conn->state = S_FREE;
+    assert(conn->sd < 0 && conn->state != S_FREE);
+    assert(!conn->sendq);
+    assert(!conn->recvq);
+    assert(!conn->watchdog);
+    conn->state = S_FREE;
 
 #ifdef HAVE_SSL
-	if (param.use_ssl)
-		SSL_free(conn->ssl);
+    if (param.use_ssl)
+        SSL_free(conn->ssl);
 #endif
 }
