0c42cd030afd445f0482ed5d4791239efca1942f
Maxim Ritter von Onciul <onciul@cs.fau.de>
2025-01-30 10:33:29 +0100

    meas: more measurements

--- measure.sh
+++ measure.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo RATLS, no freshness
-httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100 --use-ratls
+httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=1000 --num-calls=1 --use-ratls > ratls-no-fresh-1000-1
+httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=10 --num-calls=100 --use-ratls > ratls-no-fresh-10-100
 
-echo RATLS, FRESHNESS
-httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100 --use-ratls --request-freshness
+httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=1000 --num-calls=1 --use-ratls --request-freshness > ratls-freshness-1000-1
+httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=10 --num-calls=100 --use-ratls --request-freshness > ratls-freshness-10-100
 
-echo NO RATLS
-httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100
+httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=1000 --num-calls=1 > no-ratls-1000-1
+httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=10 --num-calls=100 > no-ratls-10-100
