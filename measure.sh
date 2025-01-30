cc9184f869e72385a2098f9042f0f9f69401c6b4
Maxim Ritter von Onciul <onciul@cs.fau.de>
2025-01-30 09:51:11 +0100

    fix: measure

--- measure.sh
+++ measure.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo RATLS, no freshness
-src/httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100 --ratls
+httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100 --use-ratls
 
 echo RATLS, FRESHNESS
-src/httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100 --ratls --request-freshness
+httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100 --use-ratls --request-freshness
 
 echo NO RATLS
-src/httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100
+httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100
