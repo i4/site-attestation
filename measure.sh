a4842fd02f3d7e22c0fd1243e0969a2038d4bdb7
Maxim Ritter von Onciul <onciul@cs.fau.de>
2025-01-31 15:30:33 +0100

    meas: unify args, change numbers

--- measure.sh
+++ measure.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100000 --num-calls=1 --use-ratls > ratls-no-fresh-1000-1
-httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100 --num-calls=1000 --use-ratls > ratls-no-fresh-10-100
+SHARED_ARGS="--server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3"
 
-httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100000 --num-calls=1 --use-ratls --request-freshness > ratls-freshness-1000-1
-httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100 --num-calls=1000 --use-ratls --request-freshness > ratls-freshness-10-100
+httperf ${SHARED_ARGS} --num-conns=500 --num-calls=1 --use-ratls > ratls-no-fresh-single
+httperf ${SHARED_ARGS} --num-conns=100 --num-calls=10 --use-ratls > ratls-no-fresh-10
+httperf ${SHARED_ARGS} --num-conns=100 --num-calls=100 --use-ratls > ratls-no-fresh-100
 
-httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100000 --num-calls=1 > no-ratls-1000-1
-httperf --server=i4epyc1.cs.fau.de --port=443 --ssl --ssl-protocol=TLSv1_3 --num-conns=100 --num-calls=1000 > no-ratls-10-100
+httperf ${SHARED_ARGS} --num-conns=500 --num-calls=1 --use-ratls --request-freshness > ratls-freshness-single
+httperf ${SHARED_ARGS} --num-conns=100 --num-calls=10 --use-ratls --request-freshness > ratls-freshness-10
+httperf ${SHARED_ARGS} --num-conns=100 --num-calls=100 --use-ratls --request-freshness > ratls-freshness-100
+
+httperf ${SHARED_ARGS} --num-conns=500 --num-calls=1 > no-ratls-single
+httperf ${SHARED_ARGS} --num-conns=100 --num-calls=10 > no-ratls-10
+httperf ${SHARED_ARGS} --num-conns=100 --num-calls=100 > no-ratls-100
